552db69ef4d362f7ec7997ef092fcfbdf177fdb5
Hendi Santika <hendisantika@gmail.com>
2018-01-30 06:14:19 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootehcache/SpringBootEhcacheApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEhcacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEhcacheApplication.class, args);
+    }
+}
