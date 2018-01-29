8feb45f89bde9f8e3e6b57a889f764d37046f4f6
Hendi Santika <hendisantika@gmail.com>
2018-01-30 06:19:11 +0700

    #5 - Add Office Model

---
+++ src/main/java/com/hendisantika/springbootehcache/model/Office.java
@@ -0,0 +1,42 @@
+package com.hendisantika.springbootehcache.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ehcache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/01/18
+ * Time: 06.18
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+@Table(name = "OFFICE")
+@Getter
+@Setter
+public class Office implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", updatable = false, nullable = false)
+    private Long id = null;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "ADDRESS_ID")
+    private Address address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    private Department department;
+}
