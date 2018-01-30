b75c7a1439a30bb13517a73b47e85942416098f2
Hendi Santika <hendisantika@gmail.com>
2018-01-31 05:59:44 +0700

    #13 - Modify main App

--- src/main/java/com/hendisantika/springbootehcache/SpringBootEhcacheApplication.java
+++ src/main/java/com/hendisantika/springbootehcache/SpringBootEhcacheApplication.java
@@ -2,8 +2,9 @@ package com.hendisantika.springbootehcache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = JmxAutoConfiguration.class)
 public class SpringBootEhcacheApplication {
 
     public static void main(String[] args) {
