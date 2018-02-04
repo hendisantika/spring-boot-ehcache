b2d3edf5493221cbae3316fc84f1161af0baf763
Hendi Santika <hendisantika@gmail.com>
2018-02-04 07:03:10 +0700

    #41 - Add Company Controller

---
+++ src/main/java/com/hendisantika/springbootehcache/controller/CompanyController.java
@@ -0,0 +1,77 @@
+package com.hendisantika.springbootehcache.controller;
+
+import com.hendisantika.springbootehcache.model.Company;
+import com.hendisantika.springbootehcache.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ehcache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/02/18
+ * Time: 07.02
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+    @Autowired
+    private CompanyService companyService;
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public @ResponseBody
+    List<Company> getAll() {
+        return companyService.getAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public @ResponseBody
+    Company get(@PathVariable Long id) {
+        return companyService.get(id);
+    }
+
+    @RequestMapping(value = "/filter", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public @ResponseBody
+    Company get(@RequestParam String name) {
+        return companyService.get(name);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public ResponseEntity<?> create(@RequestBody Company company) {
+        companyService.create(company);
+        HttpHeaders headers = new HttpHeaders();
+        ControllerLinkBuilder linkBuilder = linkTo(methodOn(CompanyController.class).get(company.getId()));
+        headers.setLocation(linkBuilder.toUri());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void update(@RequestBody Company company) {
+        companyService.update(company);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void delete(@PathVariable Long id) {
+        companyService.delete(id);
+    }
+}
